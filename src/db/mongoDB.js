22a7a6595c7a2670cf90e888c6dee5ed2154e338
henrique.medeiros <henrique.medeiros@tecnospeed.com.br>
2026-02-03 17:32:32 -0300

    melhorando funcao

--- src/db/mongoDB.js
+++ src/db/mongoDB.js
@@ -1,34 +1,24 @@
 import mongoose from "mongoose";
 
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).json({ error: "use POST" });
 
+  try {
+    const { id } = req.body || {};
+    if (!id) return res.status(400).json({ error: "missing id" });
 
-mongoose.connect(process.env.MONGO_URL)
-        .catch(error => console.log('Falha na conexão!', error))
+    await mongoose.connect(process.env.MONGO_URL);
 
-const connection = mongoose.connection
+    const doc = await mongoose.connection
+      .useDb("plugnotas-prod")
+      .collection("nfes")
+      .findOne(
+        { _id: new mongoose.Types.ObjectId(id) },
+        { projection: { "internal.token": 1, _id: 0 } }
+      );
 
-connection.once('open', () => console.log('Database rodando!!'))
-
-export default async function obterTokenClient(req, res) {
-    try {
-
-        const id = req.body
-
-        const token = await connection
-        .useDb("plugnotas-prod")
-        .collection("nfes")
-        .findOne(
-          { _id: new mongoose.Types.ObjectId(id) },
-          { projection: {"internal.token": 1,_id: 0}}
-        );
-
-        return res.json(token.internal.token)
-    } catch (error) {
-        return res.status(400).json({ error: "invalid request" });
-    }
+    return res.json({ token: doc?.internal?.token ?? null });
+  } catch (error) {
+    return res.status(400).json({ error: "invalid request" });
+  }
 }
-
-
-
-// const returnoMongo = await obterTokenClient('602e8b97a22ff1449debc79b');
-// console.log(returnoMongo)
